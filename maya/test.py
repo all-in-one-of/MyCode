27e92eda61cba9fe409f2b7ce3a6bdf0da22b790
Unknown <hanyuncheng@outlook.com>
2019-01-09 20:53:22 +0800

    day03

--- maya/test.py
+++ maya/test.py
@@ -1,8 +1,10 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+"""
+Created on 2017年11月11日13:19:20
+@author: qcy
+"""
 
-import sys
-from PySide2 import QtWidgets, QtCore
-from shiboken2 import wrapInstance
-import maya.OpenMayaUI as om
+import os
 
-print '\xe6\x98\xaf\xe5\x90\xa6\xe6\x9c\x89\xe9\x87\x91\xe5\xb1\x9e\xe9\x85\x8d\xe4\xbb\xb6'
+
+def findSpecifiedFile()
