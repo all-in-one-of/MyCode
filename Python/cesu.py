1b2953151c49b52e749979c841e7b810ec33d325
Unknown <hanyuncheng@outlook.com>
2019-01-24 01:34:45 +0800

    day17

--- Python/cesu.py
+++ Python/cesu.py
@@ -11,7 +11,7 @@ from pymouse import PyMouse
 from pykeyboard import PyKeyboard
 import json
 
-with open(r'C:\Users\Intime\Documents\MyCode\maya\intime\0010000000.json', 'r', encoding='utf-8') as f:
+with open(r'C:\Users\HYC\Documents\MyCode\maya\intime\0010000000.json', 'r', encoding='utf-8') as f:
     commodity_info = json.load(f)
 
 
@@ -35,6 +35,7 @@ def upfile(file):
     time.sleep(0.5)
 
     kk.tap_key(kk.enter_key)
+    time.sleep(0.5)
 
 
 # 前台开启浏览器模式
@@ -180,54 +181,52 @@ def modification(driver):
 
 
 class InTimeWebUpload():
-    def __init__(self, driver):
-        self.driver = driver
 
     def login(self):
         url = "http://cf.3dintime.com/"
-        self.driver.get(url)
-        userName = self.driver.find_element_by_name("user")  # 用户
+        driver.get(url)
+        userName = driver.find_element_by_name("user")  # 用户
         userName.send_keys("yingtai")
-        pwdName = self.driver.find_element_by_name("pwd")  # 密码
+        pwdName = driver.find_element_by_name("pwd")  # 密码
         pwdName.send_keys('YT56894!@#')
-        login_button = self.driver.find_element_by_css_selector(
+        login_button = driver.find_element_by_css_selector(
             '#loginform > form > div:nth-child(3) > div > input.btn.btn-success.radius.size-L')  # 登陆
         login_button.click()
 
     def add_commodity(self, commodity):
         with open(commodity, 'r', encoding='utf-8') as f:
             commodity_info = json.load(f)
-        b = self.driver.find_elements_by_xpath('//*[@id="menu-article"]/dt/i[2]')  # 商品管理选单
+        b = driver.find_elements_by_xpath('//*[@id="menu-article"]/dt/i[2]')  # 商品管理选单
         b[1].click()
-        c = self.driver.find_elements_by_xpath('//*[@id="menu-article"]/dd/ul/li[1]/a')  # 商品管理
+        c = driver.find_elements_by_xpath('//*[@id="menu-article"]/dd/ul/li[1]/a')  # 商品管理
         c[1].click()
-        self.driver.switch_to.frame(self.driver.find_elements_by_tag_name("iframe")[1])  # 选择商品管理frame
+        driver.switch_to.frame(driver.find_elements_by_tag_name("iframe")[1])  # 选择商品管理frame
 
-        self.driver.find_element_by_css_selector(
+        driver.find_element_by_css_selector(
             'body > div > div.cl.pd-5.bg-1.bk-gray.mt-20 > span.l > a').click()  # 添加商品
-        self.driver.switch_to.frame(self.driver.find_element_by_tag_name("iframe"))
+        driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
         self.upload_commodity(commodity_info)
 
     def revise_commodity(self, commodity):
         with open(commodity, 'r', encoding='utf-8') as f:
             commodity_info = json.load(f)
 
-        b = self.driver.find_elements_by_xpath('//*[@id="menu-article"]/dt/i[2]')  # 商品管理选单
+        b = driver.find_elements_by_xpath('//*[@id="menu-article"]/dt/i[2]')  # 商品管理选单
         b[1].click()
-        c = self.driver.find_elements_by_xpath('//*[@id="menu-article"]/dd/ul/li[1]/a')  # 商品管理
+        c = driver.find_elements_by_xpath('//*[@id="menu-article"]/dd/ul/li[1]/a')  # 商品管理
         c[1].click()
-        self.driver.switch_to.frame(self.driver.find_elements_by_tag_name("iframe")[1])  # 选择商品管理frame
-        search_name = self.driver.find_element_by_name('name')  # 按商品名查找
+        driver.switch_to.frame(driver.find_elements_by_tag_name("iframe")[1])  # 选择商品管理frame
+        search_name = driver.find_element_by_name('name')  # 按商品名查找
         search_name.send_keys(commodity_info['商品名称'])
-        search_button = self.driver.find_element_by_css_selector('body > div > form > button')  # 搜索按钮
+        search_button = driver.find_element_by_css_selector('body > div > form > button')  # 搜索按钮
         search_button.click()
-        edit_button = self.driver.find_element_by_css_selector(
+        edit_button = driver.find_element_by_css_selector(
             'body > div > div:nth-child(3) > table > tbody > tr > td.td-manage > a:nth-child(1)')  # 编辑按钮
         edit_button.click()
-        self.driver.switch_to.frame(self.driver.find_element_by_tag_name("iframe"))
-        self.upload_commodity(commodity_info, revise=True)
+        driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
+        self.upload_commodity(commodity_info)
 
-    def upload_commodity(self, commodity_info, revise=False, option_index=3):
+    def upload_commodity(self, commodity_info, revise=False):
         up_file = []
         up_image = []
 
@@ -286,74 +285,85 @@ class InTimeWebUpload():
 
         md5 = driver.find_element_by_name('md5')  # MD5
         md5.clear()
-        md5.send_keys(commodity_info['2019-01-12']['MD5码'])
+        md5.send_keys(commodity_info['规格']['2019-01-12']['MD5码'])
 
         skin_file = driver.find_element_by_name('skin_file')  # 皮肤包文件
-        skin_file.send_keys(commodity_info['2019-01-12']['材质包地址'])
+        skin_file.send_keys(commodity_info['规格']['2019-01-12']['材质包地址'])
 
         skin_file_button = driver.find_element_by_css_selector(
             '#form-add-edit > div:nth-child(17) > div.formControls.col-5 > input:nth-child(7)')  # 上传皮肤包按钮
 
-        up_file.append(skin_file_button)
+        skin_file_button.click()
+        colseAlert()
+        # up_file.append(skin_file_button)
 
         up_image_button = driver.find_element_by_xpath('//*[@id="image"]/div[2]/label')  # 图片上传按钮
+        up_image_button.click()
+        upfile(commodity_info['商品图片地址'].replace('//', '\\'))
 
-        # up_image_button = driver.find_elements_by_tag_name('label')[19]
-        up_image.append(up_image_button)
-        # up_image_button.click()
-        # upfile(commodity_info['2019-01-12']['渲染图片地址'].replace('//', '\\'))
-
-        # for i in up_image_button:
-        #     print(up_image_button.index(i),i.get_attribute('style'))
+        # up_image.append(up_image_button)
 
         add = driver.find_element_by_name('add')  # 添加规格
         if revise is False:
-            for i in range(option_index):
+            i = 0
+            for name, info in commodity_info['规格']['2019-01-12']['模型'].items():
                 add.click()
 
                 option_name = driver.find_elements_by_id('option_name')[i]  # 规格名称
                 option_name.clear()
-                option_name.send_keys(commodity_info['规格名称'])
+                option_name.send_keys(name)
 
                 option_price = driver.find_elements_by_id('option_price')[i]  # 规格价格
                 option_price.clear()
-                option_price.send_keys(commodity_info['价格'])
+                option_price.send_keys(info['价格'])
 
                 option_sku = driver.find_elements_by_id('option_sku')[i]  # 规格sku
                 option_sku.clear()
-                option_sku.send_keys(commodity_info['sku'])
-
-                option_model_file = driver.find_elements_by_name('option_model_file')[i]  # 规格模型文件
-                option_model_file.send_keys(commodity_info['2019-01-12']['模型包地址'])
-                option_model_file_button = driver.find_element_by_xpath(
-                    '//*[@id="form-add-edit"]/div[18]/div[1]/table/tbody/tr[%s]/td[5]/input[3]' % (2 + i))  # 规格模型上传按钮
+                option_sku.send_keys(info['sku'])
 
                 option_image_button = driver.find_element_by_xpath(
-                    '//*[@id="form-add-edit"]/div[18]/div[1]/table/tbody/tr[%s]/td[6]/span/div[2]/label' % (2 + i))
-                up_image.append(option_image_button)
-                up_file.append(option_model_file_button)
+                    '//*[@id="form-add-edit"]/div[18]/div[1]/table/tbody/tr[%s]/td[6]/span/div[2]/label' % (
+                                2 + i))  # 图片上传按钮
+                # up_image.append(option_image_button)
+                option_image_button.click()
+                upfile(info['渲染图片地址'].replace('//', '\\'))
+                # time.sleep(0.5)
+                option_model_file = driver.find_elements_by_name('option_model_file')[i]  # 规格模型文件
+                option_model_file.send_keys(info['模型包地址'])
+                option_model_file_button = driver.find_element_by_xpath(
+                    '//*[@id="form-add-edit"]/div[18]/div[1]/table/tbody/tr[%s]/td[5]/input[3]' % (2 + i))  # 规格模型上传按钮
+                option_model_file_button.click()
+                colseAlert()
 
-        # status1 = driver.find_elements_by_tag_name('label')[23]  # 上架
-        # status0 = driver.find_elements_by_tag_name('label')[24]  # 下架
-        # position = driver.find_element_by_id('position')  # 排序
-        # position.send_keys(commodity_info['排序'])
-        # option_image_button.click()
-        # upfile(commodity_info['2019-01-12']['渲染图片地址'].replace('//', '\\'))
-        # skin_file_button.click()
-        # colseAlert()
-        # option_model_file_button.click()
-        # colseAlert()
-        # submit = driver.find_element_by_css_selector('#form-add-edit > div:nth-child(22) > div > input')  # 提交
-        # submit.click()
+                # up_file.append(option_model_file_button)
+                i += 1
+        status1 = driver.find_element_by_xpath('//*[@id="form-add-edit"]/div[20]/div[1]/div[1]/label')  # 上架
+        status0 = driver.find_element_by_xpath('//*[@id="form-add-edit"]/div[20]/div[1]/div[2]/label')  # 下架
+
+        position = driver.find_element_by_id('position')  # 排序
+        position.clear()
+        position.send_keys(commodity_info['排序'])
+
+        # for up in up_image:
+        #     up.click()
+        #     upfile(commodity_info['商品图片地址'].replace('//', '\\'))
+
+        # for up in up_file:
+        #     up.click()
+        #     colseAlert()
+
+
+        submit = driver.find_element_by_css_selector('#form-add-edit > div:nth-child(22) > div > input')  # 提交
+        submit.click()
 
 
 # 方法主入口
 if __name__ == '__main__':
     # 加启动配置
     driver = openChrome()
-    it = InTimeWebUpload(driver)
+    it = InTimeWebUpload()
     it.login()
-    it.add_commodity(r'C:\Users\Intime\Documents\MyCode\maya\intime\0010000000.json')
+    it.revise_commodity(r"D:\test\0010000000\0010000000.json")
 
     # modification(driver)
 
