0fbb39cd9656b263eab1d2e0dc4b6039efd31852
Unknown <hanyuncheng@outlook.com>
2019-03-21 18:42:37 +0800

    16

--- IntimeTool/test.py
+++ IntimeTool/test.py
@@ -5,6 +5,8 @@
 @Author  : Intime
 @Software: PyCharm
 """
+import subprocess
+
 from IntimeTool.usual import *
 
 # ab = r"F:\Share\createAssetBundels\AssetBundles"
@@ -36,100 +38,17 @@ from IntimeTool.usual import *
 #
 #     goodsInfo['specifications'].append(specification)
 #     writeJson(goodsJson,goodsInfo)
-from requests import session, Request, get
-import re
-from bs4 import BeautifulSoup as bs  # Beautiful Soup是一个可以从HTML或XML文件中提取结构化数据的Python库
 
 
-def optionNum():
-    sku = []
-    skuList = soup.find_all(id='option_sku')
-    if len(skuList) == 1:
-        sku.append(skuList[0]['value'])
-        return sku
-    else:
+from subprocess import Popen, PIPE, STDOUT
+import os
 
-        for i in skuList:
-            sku.append(i['value'])
-        return sku
+currentPath = os.path.dirname(os.path.realpath(__file__))
+command = r'SimplygonBatch --Input F:\Share\simplygon\inputDir --Output F:\Share\simplygon\outputDir --Spl F:\Share\simplygon\splDir --Temp F:\Share\simplygon\temp --Verbose --OutputFileFormat .fbx'
+p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True, cwd=currentPath)
+output, errors = p.communicate()
 
+if errors:
+    print(errors)
 
-def optionName():
-    name = []
-    skuList = soup.find_all(id='option_name')
-    if len(skuList) == 1:
-        name.append(skuList[0]['value'])
-        return name
-    else:
-
-        for i in skuList:
-            name.append(i['value'])
-        return name
-
-
-def goodsSKU(sku):
-    sku = sku[0]
-    if len(sku) > 4:
-        return sku[:4]
-    else:
-        return sku
-
-
-def goodsName():
-    return soup.find(id='name')['value']
-
-
-
-
-url = "http://cf.3dintime.com/index/index"
-
-session_req = session()
-
-postData = {
-    "user": "yingtai",
-    "pwd": "YT56894!@#"
-}
-
-# 需要登录的URL
-login_url = "http://cf.3dintime.com/admin_user/public/login/#"
-# PreparedRequest请求预处理
-req = Request(
-    'post',
-    login_url,
-    data=postData,
-    headers=dict(referer=login_url)
-)
-prepped = req.prepare()
-
-# 将处理的请求参数通过session请求对象发送过去
-resp = session_req.send(prepped)
-num = []
-for i in range(7):
-    a = session_req.get(
-        r'http://cf.3dintime.com/product/product/index?start_at=&end_at=&status=&seller_name=&brand_name=%E8%BD%A9&name=&page=' + '%s' % (
-                    i + 1))
-
-
-
-    goodslist = re.findall(r'<a href="javascript:layer_full_add_edit(.*?);">编辑</a>', a.text, re.S)
-
-    for goods in goodslist:
-        goodsEdit = tuple(eval(goods))[1]
-
-        b = session_req.get(goodsEdit)
-
-        c = b.content
-
-        soup = bs(c, "html.parser")
-
-        goodsinfo = ((goodsName(), goodsSKU(optionNum()),optionName()))
-        num.append(goodsinfo)
-
-print(len(num))
-writeJson(r'C:\Users\Intime\Documents\MyCode\IntimeTool\rdx.json',num)
-
-# 用BeautifulSoup处理登录之后返回的数据
-# soup=bs(resp.content,"html.parser")
-
-# 打印输出
-# print(soup.prettify())
+print(output.decode(encoding='gbk'))
--- IntimeTool/usual.py
+++ IntimeTool/usual.py
@@ -14,6 +14,107 @@ from PIL import Image, ImageChops, ImageOps, ImageFilter
 import json
 import time
 import hashlib
+import wmi
+c = wmi.WMI()
+#处理器
+def printCPU():
+    tmpdict = {}
+    tmpdict["CpuCores"] = 0
+    for cpu in c.Win32_Processor():
+        tmpdict["cpuid"] = cpu.ProcessorId.strip()
+        tmpdict["CpuType"] = cpu.Name
+        tmpdict['systemName'] = cpu.SystemName
+        try:
+            tmpdict["CpuCores"] = cpu.NumberOfCores
+        except:
+            tmpdict["CpuCores"] += 1
+        tmpdict["CpuClock"] = cpu.MaxClockSpeed
+        tmpdict['DataWidth'] = cpu.DataWidth
+    print(tmpdict)
+    return  tmpdict
+
+#主板
+def printMain_board():
+    boards = []
+    # print len(c.Win32_BaseBoard()):
+    for board_id in c.Win32_BaseBoard():
+        tmpmsg = {}
+        tmpmsg['UUID'] = board_id.qualifiers['UUID'][1:-1]   #主板UUID,有的主板这部分信息取到为空值，ffffff-ffffff这样的
+        tmpmsg['SerialNumber'] = board_id.SerialNumber                #主板序列号
+        tmpmsg['Manufacturer'] = board_id.Manufacturer       #主板生产品牌厂家
+        tmpmsg['Product'] = board_id.Product                 #主板型号
+        boards.append(tmpmsg)
+    print(boards)
+    return boards
+
+#BIOS
+def printBIOS():
+    bioss = []
+    for bios_id in c.Win32_BIOS():
+        tmpmsg = {}
+        tmpmsg['BiosCharacteristics'] = bios_id.BiosCharacteristics   #BIOS特征码
+        tmpmsg['version'] = bios_id.Version                           #BIOS版本
+        tmpmsg['Manufacturer'] = bios_id.Manufacturer.strip()                 #BIOS固件生产厂家
+        tmpmsg['ReleaseDate'] = bios_id.ReleaseDate                   #BIOS释放日期
+        tmpmsg['SMBIOSBIOSVersion'] = bios_id.SMBIOSBIOSVersion       #系统管理规范版本
+        bioss.append(tmpmsg)
+    print(bioss)
+    return bioss
+
+#硬盘
+def printDisk():
+    disks = []
+    for disk in c.Win32_DiskDrive():
+        # print disk.__dict__
+        tmpmsg = {}
+        tmpmsg['SerialNumber'] = disk.SerialNumber.strip()
+        tmpmsg['DeviceID'] = disk.DeviceID
+        tmpmsg['Caption'] = disk.Caption
+        tmpmsg['Size'] = disk.Size
+        tmpmsg['UUID'] = disk.qualifiers['UUID'][1:-1]
+        disks.append(tmpmsg)
+    for d in disks:
+        print(d)
+    return disks
+
+#内存
+def printPhysicalMemory():
+    memorys = []
+    for mem in c.Win32_PhysicalMemory():
+        tmpmsg = {}
+        tmpmsg['UUID'] = mem.qualifiers['UUID'][1:-1]
+        tmpmsg['BankLabel'] = mem.BankLabel
+        tmpmsg['SerialNumber'] = mem.SerialNumber.strip()
+        tmpmsg['ConfiguredClockSpeed'] = mem.ConfiguredClockSpeed
+        tmpmsg['Capacity'] = mem.Capacity
+        tmpmsg['ConfiguredVoltage'] = mem.ConfiguredVoltage
+        memorys.append(tmpmsg)
+    for m in memorys:
+        print(m)
+    return memorys
+
+#电池信息，只有笔记本才会有电池选项
+def printBattery():
+    isBatterys = False
+    for b in c.Win32_Battery():
+        isBatterys = True
+    return isBatterys
+
+#网卡mac地址：
+def printMacAddress():
+    macs = []
+    for n in  c.Win32_NetworkAdapter():
+        mactmp = n.MACAddress
+        if mactmp and len(mactmp.strip()) > 5:
+            tmpmsg = {}
+            tmpmsg['MACAddress'] = n.MACAddress
+            tmpmsg['Name'] = n.Name
+            tmpmsg['DeviceID'] = n.DeviceID
+            tmpmsg['AdapterType'] = n.AdapterType
+            tmpmsg['Speed'] = n.Speed
+            macs.append(tmpmsg)
+    print(macs)
+    return macs
 
 def getMd5(file):
     myhash = hashlib.md5()
@@ -119,6 +220,91 @@ def writeJson(jsonPath, info):
 def detectionChinese(text):
     b = re.compile(u'[\u4e00-\u9fa5]+').search(text)
     return b
+
+def refreshMerchantInfo():
+    from requests import session, Request
+    import re
+    from bs4 import BeautifulSoup as bs  # Beautiful Soup是一个可以从HTML或XML文件中提取结构化数据的Python库
+
+    def optionNum():
+        sku = []
+        skuList = soup.find_all(id='option_sku')
+        if len(skuList) == 1:
+            sku.append(skuList[0]['value'])
+            return sku
+        else:
+
+            for i in skuList:
+                sku.append(i['value'])
+            return sku
+
+    def optionName():
+        name = []
+        skuList = soup.find_all(id='option_name')
+        if len(skuList) == 1:
+            name.append(skuList[0]['value'])
+            return name
+        else:
+
+            for i in skuList:
+                name.append(i['value'])
+            return name
+
+    def goodsSKU(sku):
+        sku = sku[0]
+        if len(sku) > 4:
+            return sku[:4]
+        else:
+            return sku
+
+    def goodsName():
+        return soup.find(id='name')['value']
+
+    url = "http://cf.3dintime.com/index/index"
+
+    session_req = session()
+
+    postData = {
+        "user": "yingtai",
+        "pwd": "YT56894!@#"
+    }
+
+    # 需要登录的URL
+    login_url = "http://cf.3dintime.com/admin_user/public/login/#"
+    # PreparedRequest请求预处理
+    req = Request(
+        'post',
+        login_url,
+        data=postData,
+        headers=dict(referer=login_url)
+    )
+    prepped = req.prepare()
+
+    # 将处理的请求参数通过session请求对象发送过去
+    resp = session_req.send(prepped)
+    num = []
+    for i in range(7):
+        a = session_req.get(
+            r'http://cf.3dintime.com/product/product/index?start_at=&end_at=&status=&seller_name=&brand_name=%E8%BD%A9&name=&page=' + '%s' % (
+                    i + 1))
+
+        goodslist = re.findall(r'<a href="javascript:layer_full_add_edit(.*?);">编辑</a>', a.text, re.S)
+
+        for goods in goodslist:
+            goodsEdit = tuple(eval(goods))[1]
+
+            b = session_req.get(goodsEdit)
+
+            c = b.content
+
+            soup = bs(c, "html.parser")
+
+            goodsinfo = ((goodsName(), goodsSKU(optionNum()), optionName()))
+            num.append(goodsinfo)
+
+    print(len(num))
+    writeJson(r'C:\Users\Intime\Documents\MyCode\IntimeTool\rdx.json', num)
+
 
 # # with open(os.path.join(FilePath,'newGoods.json'),'w',encoding='utf-8') as f:
 # for dir in os.listdir(FilePath):
