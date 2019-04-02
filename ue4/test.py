46cad043a1914daca37c7e79d3ea844325d90b9b
Unknown <hanyuncheng@outlook.com>
2019-04-03 00:25:59 +0800

    0.

--- ue4/test.py
+++ ue4/test.py
@@ -1,27 +1,9 @@
 import os
-from unreal_engine.classes import PyFbxFactory
+from unreal_engine.classes import PyFbxFactory, MaterialFactoryNew, TextureFactory
 import json
+
+
 # instantiate a new factory
-fbx_factory = PyFbxFactory()
-
-
-def fbx_import(slicer_fbx):
-    # build the path for the fbx file
-    # kaiju_assets_dir = os.path.join(os.path.expanduser('~/Desktop'), 'Kaiju_Assets/Slicer')
-    #
-    # slicer_fbx = os.path.join(kaiju_assets_dir, 'slicer.fbx')
-
-    # configure the factory
-    fbx_factory.ImportUI.bCreatePhysicsAsset = False
-    fbx_factory.ImportUI.bImportMaterials = True
-    fbx_factory.ImportUI.bImportTextures = True
-    fbx_factory.ImportUI.bImportAnimations = False
-    # scale the mesh (the Kaiju is 30 meters high !)
-    # fbx_factory.ImportUI.SkeletalMeshImportData.ImportUniformScale = 0.1
-
-    # import the mesh
-    slicer_mesh = fbx_factory.factory_import_object(
-        slicer_fbx, '/Game/Kaiju/Slicer')
 
 
 def findSpecifiedFile(path, suffix=''):
@@ -39,8 +21,75 @@ def findSpecifiedFile(path, suffix=''):
     return _file
 
 
-# for i in findSpecifiedFile(r'D:\test','.fbx'):
-#     fbx_import(i)
-fbx = r"F:\Share\simplygon\inputDir\00000000.fbx"
-fbx_import(fbx)
-aaa
+def fbxImport(fbx, scale=1):
+    fbx_factory = PyFbxFactory()
+    # build the path for the fbx file
+
+    # configure the factory
+    fbx_factory.ImportUI.bCreatePhysicsAsset = False
+    fbx_factory.ImportUI.bImportMaterials = False
+    fbx_factory.ImportUI.bImportTextures = False
+    fbx_factory.ImportUI.bImportAnimations = False
+    # scale the mesh (the Kaiju is 30 meters high !)
+    fbx_factory.ImportUI.SkeletalMeshImportData.ImportUniformScale = scale
+
+    # import the mesh
+    slicer_mesh = fbx_factory.factory_import_object(
+        fbx, '/Game/Kaiju/Slicer')
+
+
+def createMaterial():
+    material_factory = MaterialFactoryNew()
+
+    material_blades = material_factory.factory_create_new('/Game/Kaiju/Slicer/Blades_Material')
+
+    material_body = material_factory.factory_create_new('/Game/Kaiju/Slicer/Body_Material')
+
+
+def textureImport():
+    # instantiate a factory for importing textures
+    texture_factory = TextureFactory()
+    # ensures textures are overwritten (2 means, YesAll, defined in Engine/Source/Runtime/Core/Public/GenericPlatform/GenericPlatformMisc.h, EAppReturnType::YesAll)
+    texture_factory.OverwriteYesOrNoToAllState = 2
+
+    slicer_blade_texture_base_color_tga = os.path.join(kaiju_assets_dir, 'Textures/slicer_blade_BaseColor.tga')
+    slicer_blade_texture_base_color = texture_factory.factory_import_object(slicer_blade_texture_base_color_tga,
+                                                                            '/Game/Kaiju/Slicer/Textures')
+
+    slicer_blade_texture_normal_tga = os.path.join(kaiju_assets_dir, 'Textures/slicer_blade_Normal.tga')
+    slicer_blade_texture_normal = texture_factory.factory_import_object(slicer_blade_texture_normal_tga,
+                                                                        '/Game/Kaiju/Slicer/Textures')
+
+    slicer_blade_texture_emissive_tga = os.path.join(kaiju_assets_dir, 'Textures/slicer_blade_Emissive.tga')
+    slicer_blade_texture_emissive = texture_factory.factory_import_object(slicer_blade_texture_emissive_tga,
+                                                                          '/Game/Kaiju/Slicer/Textures')
+
+    # orm stands for OcclusionRoughnessMetallic
+    slicer_blade_texture_orm_tga = os.path.join(kaiju_assets_dir,
+                                                'Textures/slicer_blade_OcclusionRoughnessMetallic.tga')
+    slicer_blade_texture_orm = texture_factory.factory_import_object(slicer_blade_texture_orm_tga,
+                                                                     '/Game/Kaiju/Slicer/Textures')
+
+    slicer_texture_base_color_tga = os.path.join(kaiju_assets_dir, 'Textures/slicer_BaseColor.tga')
+    slicer_texture_base_color = texture_factory.factory_import_object(slicer_texture_base_color_tga,
+                                                                      '/Game/Kaiju/Slicer/Textures')
+
+    slicer_texture_normal_tga = os.path.join(kaiju_assets_dir, 'Textures/slicer_Normal.tga')
+    slicer_texture_normal = texture_factory.factory_import_object(slicer_texture_normal_tga,
+                                                                  '/Game/Kaiju/Slicer/Textures')
+
+    slicer_texture_emissive_tga = os.path.join(kaiju_assets_dir, 'Textures/slicer_Emissive.tga')
+    slicer_texture_emissive = texture_factory.factory_import_object(slicer_texture_emissive_tga,
+                                                                    '/Game/Kaiju/Slicer/Textures')
+
+    # orm stands for OcclusionRoughnessMetallic
+    slicer_texture_orm_tga = os.path.join(kaiju_assets_dir, 'Textures/slicer_OcclusionRoughnessMetallic.tga')
+    slicer_texture_orm = texture_factory.factory_import_object(slicer_texture_orm_tga, '/Game/Kaiju/Slicer/Textures')
+
+
+kaiju_assets_dir = os.path.join(os.path.expanduser('~/Desktop'), 'Kaiju_Assets/Slicer')
+
+slicer_fbx = os.path.join(kaiju_assets_dir, 'slicer.fbx')
+# fbxImport(slicer_fbx, 0.1)
+createMaterial()
+textureImport()
